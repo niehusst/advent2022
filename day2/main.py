ccf04b0606c380abdf89128922da581620e83968
niehusst <liamniehusstaab@gmail.com>
2022-12-02 20:47:07 -0700

    day 2

---
+++ day2/main.py
@@ -0,0 +1,80 @@
+# part 1
+
+def moveScore(move):
+    if move == 'X':
+        return 1
+    elif move == 'Y':
+        return 2
+    else:
+        return 3
+
+def outcomeScore(enemyMove, myMove):
+    if enemyMove == 'A' and myMove == 'X' or \
+        enemyMove == 'B' and myMove == 'Y' or \
+        enemyMove == 'C' and myMove == 'Z':
+        return 3
+    elif enemyMove == 'A' and myMove == 'Y' or \
+        enemyMove == 'B' and myMove == 'Z' or \
+        enemyMove == 'C' and myMove == 'X':
+        return 6
+    else:
+        return 0
+
+def calcScore(enemyMove, myMove):
+    return moveScore(myMove) + outcomeScore(enemyMove, myMove)
+
+with open('input.txt') as f:
+    score = 0
+    
+    for line in f:
+        enemyMove, myMove = line.strip().split(' ')
+        if myMove == None:
+            continue
+        score += calcScore(enemyMove, myMove)
+
+    print(f"final score: {score}")
+
+# part 2
+
+def getMyMove(enemyMove, outcome):
+    if outcome == 'X' and enemyMove == 'A' or \
+        outcome == 'Z' and enemyMove == 'B' or \
+        outcome == 'Y' and enemyMove == 'C':
+        return 'C'
+    elif outcome == 'X' and enemyMove == 'B' or \
+        outcome == 'Z' and enemyMove == 'C' or \
+        outcome == 'Y' and enemyMove == 'A':
+        return 'A'
+    else:
+        return 'B'
+
+def nmoveScore(move):
+    if move == 'A':
+        return 1
+    elif move == 'B':
+        return 2
+    else:
+        return 3
+
+def noutcomeScore(outcome):
+    if outcome == 'X':
+        return 0
+    elif outcome == 'Y':
+        return 3
+    else:
+        return 6
+
+def ncalcScore(enemyMove, outcome):
+    myMove = getMyMove(enemyMove, outcome)
+    return nmoveScore(myMove) + noutcomeScore(outcome)
+
+with open('input.txt') as f:
+    score = 0
+    
+    for line in f:
+        enemyMove, outcome = line.strip().split(' ')
+        if myMove == None:
+            continue
+        score += ncalcScore(enemyMove, outcome)
+
+    print(f"final score: {score}")
